4b9e982ea4f739c77044d9e89cbe0ad348aa67af
Александр Тарасов <alexxtarasov2015@gmail.com>
2020-10-29 21:49:33 +0200

    firstInit

---
+++ game2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+#define UP 72
+#define DOWN 80
+#define RIGHT 75
+#define LEFT 77
+#define SPACE 32
+void Show(int** field, int a) {
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < a; j++)
+		{
+			cout << "0" << ends;
+		}
+		cout << endl;
+	}
+}
+
+
+void MovePlus(int** field, int a) {
+
+}
+
+void MoveToPlus(int** field, int a) {
+
+}
+
+void Initialize(int** field, int a) {
+
+}
+int main()
+{
+	int N = 40;
+	int** field = new int* [50];
+	Show(field, N);
+}
